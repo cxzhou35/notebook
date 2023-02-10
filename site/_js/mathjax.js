3c53704392863c230287fe2b541ee48c1e13559f
Xuer04 <zjajzcx3566@163.com>
2023-02-10 12:14:09 +0800

    [Default] Content Synchronization

--- site/_js/mathjax.js
+++ site/_js/mathjax.js
@@ -1,23 +1,3 @@
-// window.MathJax = {
-//   tex2jax: {
-//     inlineMath: [ ["\\(","\\)"] ],
-//     displayMath: [ ["\\[","\\]"] ]
-//   },
-//   TeX: {
-//     TagSide: "left",
-//     TagIndent: ".8em",
-//     MultLineWidth: "85%",
-//     equationNumbers: {
-//       autoNumber: "AMS",
-//     },
-//     unicode: {
-//       fonts: "STIXGeneral,'Arial Unicode MS'"
-//     }
-//   },
-//   displayAlign: "left",
-//   showProcessingMessages: false,
-//   messageStyle: "none"
-// };
 window.MathJax = {
   tex: {
     inlineMath: [["\\(", "\\)"]],
