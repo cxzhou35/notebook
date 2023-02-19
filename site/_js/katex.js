05a751495b49cea5a36f5db4e0b39789168d9c40
Xuer04 <zjajzcx3566@163.com>
2023-02-19 10:08:22 +0800

    [Default] Content Synchronization

--- site/_js/katex.js
+++ site/_js/katex.js
@@ -6,33 +6,33 @@
             tex;
 
         for (var i = 0; i < maths.length; i++) {
-          tex = maths[i].textContent || maths[i].innerText;
-          if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
-            katex.render(tex.slice(2, -2), maths[i], {'displayMode': false});
-          } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
-            katex.render(tex.slice(2, -2), maths[i], {'displayMode': true});
-          }
+            tex = maths[i].textContent || maths[i].innerText;
+            if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false });
+            } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true });
+            }
         }
     });
 
     (function () {
-      var onReady = function onReady(fn) {
-        if (document.addEventListener) {
-          document.addEventListener("DOMContentLoaded", fn);
-        } else {
-          document.attachEvent("onreadystatechange", function () {
-            if (document.readyState === "interactive") {
-              fn();
+        var onReady = function onReady(fn) {
+            if (document.addEventListener) {
+                document.addEventListener("DOMContentLoaded", fn);
+            } else {
+                document.attachEvent("onreadystatechange", function () {
+                    if (document.readyState === "interactive") {
+                        fn();
+                    }
+                });
             }
-          });
-        }
-      };
+        };
 
-      onReady(function () {
-        if (typeof katex !== "undefined") {
-          katexMath();
-        }
-      });
+        onReady(function () {
+            if (typeof katex !== "undefined") {
+                katexMath();
+            }
+        });
     })();
 
-    }());
+}());
