2ff6f0f830da4c63cf58701aa64e42eaa039b8e6
Xuer04 <zjajzcx3566@163.com>
2023-02-21 13:57:05 +0800

    Deployed e42506a with MkDocs version: 1.4.2

--- _js/tablesort.js
+++ _js/tablesort.js
@@ -1,7 +1,7 @@
 document$.subscribe(function() {
-    var tables = document.querySelectorAll("article table:not([class])")
-    tables.forEach(function(table) {
-      new Tablesort(table)
-    })
+  var tables = document.querySelectorAll("article table:not([class])")
+  tables.forEach(function(table) {
+    new Tablesort(table)
   })
-  
+})
+
