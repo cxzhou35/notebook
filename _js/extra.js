c38f04e430c06cff5bf8a8bdb55b3ce926eff9f8
Xuer04 <zjajzcx3566@163.com>
2023-04-25 16:35:02 +0800

    Deployed dd64bde with MkDocs version: 1.4.2

--- _js/extra.js
+++ _js/extra.js
@@ -1,13 +1,13 @@
-const updateScheme = e => {
-    var giscus = document.querySelector(".giscus-frame");
-    var a = localStorage.getItem('data-md-color-scheme');
-    var theme = a === "default" ? "light" : "dark";
-    // alert(a + " -> " + theme);
-    giscus.contentWindow.postMessage(
-        { giscus: { setConfig: { theme } } },
-        "https://giscus.app"
-    )
-}
+// const updateScheme = e => {
+//     var giscus = document.querySelector(".giscus-frame");
+//     var a = localStorage.getItem('data-md-color-scheme');
+//     var theme = a === "default" ? "light" : "dark";
+//     // alert(a + " -> " + theme);
+//     giscus.contentWindow.postMessage(
+//         { giscus: { setConfig: { theme } } },
+//         "https://giscus.app"
+//     )
+// }
 
 window.MathJax = {
     tex: {
