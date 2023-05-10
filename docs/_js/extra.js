c0025c940037aa9781e52aa1c7ab7b1028afe020
cxzhou35 <zjajzcx3566@163.com>
2023-05-10 13:15:56 +0800

    [+] update readme(add screenshot)

--- docs/_js/extra.js
+++ docs/_js/extra.js
@@ -1,27 +1,16 @@
-// const updateScheme = e => {
-//     var giscus = document.querySelector(".giscus-frame");
-//     var a = localStorage.getItem('data-md-color-scheme');
-//     var theme = a === "default" ? "light" : "dark";
-//     // alert(a + " -> " + theme);
-//     giscus.contentWindow.postMessage(
-//         { giscus: { setConfig: { theme } } },
-//         "https://giscus.app"
-//     )
-// }
-
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
-  
-  document$.subscribe(() => { 
-    MathJax.typesetPromise()
-  })
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
